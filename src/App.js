0a5b68e8c6a6fb7f711e6bf87dec09b9eb2b172b
zewcro <theorousseau75@gmail.com>
2022-01-18 16:49:19 +0100

    Trying building some go apis
    A short go api who return informations about the server who running

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Movies from "./Components/Movies";
+import Home from "./Components/Home";
+import Admin from "./Components/Admin";
+import {
+  useParams,
+  useRouteMatch,
+} from "react-router-dom/cjs/react-router-dom.min";
+import Categories from "./Components/Categories";
+import OneMovie from "./Components/OneMovie";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">Go Watch a Movie!</h1>
+          <hr className="mb-3"></hr>
+        </div>
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/by-category">Categories</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/admin">Manage Catalogue</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col-md-10">
+            <Switch>
+              <Route path="/movies/:id" component={OneMovie} />
+              <Route exact path="/by-category">
+                <CategoryPage />
+              </Route>
+              <Route
+                exact
+                path="/by-category/drama"
+                render={(props) => <Categories {...props} title={`Drama`} />}
+              />
+              <Route
+                exact
+                path="/by-category/comedy"
+                render={(props) => <Categories {...props} title={`Comedy`} />}
+              />
+              <Route path="/movies">
+                <Movies />
+              </Route>
+              <Route path="/admin">
+                <Admin />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function CategoryPage() {
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Categories</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/drama`}>Drama</Link>
+        </li>
+        <li>
+          <Link to={`${url}/comedy`}>Comedy</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
